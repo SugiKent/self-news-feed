b1b43dd15685dd5951343cad8c9a78c655d5ee9d
Kento Sugita <ynmk1995@gmail.com>
2025-03-30 22:39:16 +0900

    fix

--- scripts/fetch-repos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -s -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "https://api.github.com/user/subscriptions" | \
-  jq '[.[] | select(.private == false) | {name: .full_name, description: .description, url: .html_url}]' > \
-  self-news-feed/src/data/repos.json
